6f6c15ab542ef0a0e122ee44375b08a58f4d2599
Javier <carlos8a_93@yahoo.com>
2021-10-08 20:58:12 -0600

    Agregando otro cambio a ver_constancia1.php

--- ver_constancia1.php
+++ ver_constancia1.php
@@ -590,21 +590,21 @@ class PDF extends FPDF{
             $this->SetLineWidth(.5);
             $this->SetFillColor(255,255,255);
 
-            $this->Image($this->infoext['logo'],$set_x-55,$set_y+4,120,45);
+            //$this->Image($this->infoext['logo'],$set_x-55,$set_y+4,120,45);
             $this->SetDrawColor(0,0,0);
             $this->SetFont('Courier', 'B', 19);
             $this->SetTextColor(25, 65, 96);
             $this->setY($set_y);
             //$this->Cell(160,7,utf8_decode($this->infoext['nombre_empresa']),0,1,'L');
-            $set_y +=7;
+            $set_y+=15;
             $this->setY($set_y);
             $this->SetFont('Courier', 'B', 16);
             $this->Cell(160,7,utf8_decode("Dr. ".$this->infoext['nombre_doctor']),0,1,'R');
-            /*$set_y+=7;
+            $set_y+=10;
             $this->setY($set_y);
             $this->SetX(10);
             $this->SetFont('Courier', 'B', 18);
-            $this->Cell(135,7,"CONSTANCIA MEDICA",0,1,'R');*/
+            $this->Cell(135,7,"CONSTANCIA MEDICA",0,1,'R');
 
             $texto_poner = "El infrascrito  médico, hace constar que ".Mayu($this->infoext['nombre_paciente']);
             $texto_poner.= " de ".$this->infoext['edad']." años de edad  consulto este día esta clínica con";
